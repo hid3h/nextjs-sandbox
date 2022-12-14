a0e74e7ee8cf2776626a19976c1dec047a2af20a
hid3h <razyetts@hotmail.co.jp>
2022-12-14 21:27:25 +0900

    ディレクトリ名変更

--- next-auth-line-sandbox/src/pages/api/auth/[...nextauth].ts
+++ next-auth-line-sandbox/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,5 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
-import LineProvider from "next-auth/providers/line";
+import LineProvider from "next-auth/providers/line.js";
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
