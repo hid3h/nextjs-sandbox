ef1a43bcedd0f11bf40023b168b5d6c5bf806ef3
hid3h <razyetts@hotmail.co.jp>
2023-08-01 10:13:46 +0900

    firebaseを使ったsignUp,signInができるまで

--- firebase-auth-sandbox/src/pages/index.tsx
+++ firebase-auth-sandbox/src/pages/index.tsx
@@ -1,68 +1,56 @@
-import { getSession, signIn, signOut, useSession } from "next-auth/react";
-import Head from "next/head";
-import Link from "next/link";
-import { api } from "~/utils/api";
 import { FirebaseOptions, initializeApp } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { api } from "~/utils/api";
 
 export default function Home() {
-  // const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   const firebaseConfig: FirebaseOptions = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   };
   initializeApp(firebaseConfig);
 
-  const auth = getAuth();
-  const email = "";
-  const password = "";
-  // createUserWithEmailAndPassword(auth, email, password)
-  //   .then((userCredential) => {
-  //     // Signed in
-  //     const user = userCredential.user;
-  //     console.log("user", user);
-  //   })
-  //   .catch((error) => {
-  //     const errorCode = error.code;
-  //     const errorMessage = error.message;
-  //     console.log("errorCode", errorCode);
-  //     console.log("errorMessage", errorMessage);
-  //   });
+  const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
-  const handleSignIn = () => {
-    signInWithEmailAndPassword(auth, email, password)
-      .then(async (userCredential) => {
-        // Signed in
+  const auth = getAuth();
+  const email = process.env.NEXT_PUBLIC_EXAMPLE_EMAIL || "";
+  const password = process.env.NEXT_PUBLIC_EXAMPLE_PASSWORD || "";
+
+  const handleSignUp = async () => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
         const user = userCredential.user;
-        console.log("signin成功");
-        const idToken = await user.getIdToken();
-        console.log("idToken", idToken);
-        const result = await signIn("credentials", {
-          email: "hoge@example.com",
-          password: "hogehoge",
-          redirect: false,
-        });
-        console.log("result", result);
+        console.log("user", user);
       })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
-        console.log("signin失敗");
         console.log("errorCode", errorCode);
         console.log("errorMessage", errorMessage);
       });
   };
 
-  getSession().then((session) => {
-    console.log("session", session);
-  });
+  const handleSignIn = async () => {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const user = userCredential.user;
+    console.log("user.emailVerified", user.emailVerified);
+  };
 
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <div>
+        <button onClick={handleSignUp}>Sign up</button>
+      </div>
+      <div>
         <button onClick={handleSignIn}>Sign in</button>
-      </main>
+      </div>
     </>
   );
 }
--- firebase-auth-sandbox/src/server/api/routers/example.ts
+++ firebase-auth-sandbox/src/server/api/routers/example.ts
@@ -1,24 +1,26 @@
 import { z } from "zod";
-import {
-  createTRPCRouter,
-  publicProcedure,
-  protectedProcedure,
-} from "~/server/api/trpc";
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import * as admin from "firebase-admin";
 
 export const exampleRouter = createTRPCRouter({
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
+    .query(async ({ input }) => {
+      if (!admin.apps[0]) {
+        await admin.initializeApp({
+          credential: admin.credential.cert({
+            projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+          }),
+        });
+      }
+      const authAdmin = admin.auth();
+      const email = process.env.NEXT_PUBLIC_EXAMPLE_EMAIL || "";
+      // const link = await authAdmin.generateEmailVerificationLink(email);
+      // console.log("link", link);
       return {
         greeting: `Hello ${input.text}`,
       };
     }),
-
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
-  }),
-
-  getSecretMessage: protectedProcedure.query(() => {
-    return "you can now see this secret message!";
-  }),
 });
